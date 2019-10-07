070b685261ad41c7ae7c001fd1dbfd1f13b1d7cc
mccabmic <mccabmic@oregonstate.edu>
2019-10-07 18:14:10 -0500

    refactor minion switch to minionEffect()

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -871,54 +871,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
         return 0;
 
     case minion:
-        //+1 action
-        state->numActions++;
-
-        //discard card from hand
-        discardCard(handPos, currentPlayer, state, 0);
-
-		if (choice1)
-        {
-            state->coins = state->coins + 2;
-        }
-        else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
-        {
-            //discard hand
-            while(numHandCards(state) > 0)
-            {
-                discardCard(handPos, currentPlayer, state, 0);
-            }
-
-            //draw 4
-            for (i = 0; i < 4; i++)
-            {
-                drawCard(currentPlayer, state);
-            }
-
-            //other players discard hand and redraw if hand size > 4
-            for (i = 0; i < state->numPlayers; i++)
-            {
-                if (i != currentPlayer)
-                {
-                    if ( state->handCount[i] > 4 )
-                    {
-                        //discard hand
-                        while( state->handCount[i] > 0 )
-                        {
-                            discardCard(handPos, i, state, 0);
-                        }
-
-                        //draw 4
-                        for (j = 0; j < 4; j++)
-                        {
-                            drawCard(i, state);
-                        }
-                    }
-                }
-            }
-
-        }
-        return 0;
+        return minionEffect(choice1, choice2, state, handPos);
 
     case steward:
         if (choice1 == 1)
@@ -1351,6 +1304,59 @@ int baronEffect(int choice1, struct gameState *state)
     return 0;
 
 }
+
+int minionEffect(int choice1, int choice2, struct gameState *state, int handPos)
+{
+    //+1 action
+    state->numActions++;
+
+    int currentPlayer = whoseTurn(state);
+    discardCard(handPos, currentPlayer, state, 0);
+
+    if (choice1)
+    {
+        state->coins = state->coins + 2;
+    }
+    else if (choice2)   //discard hand, redraw 4, other players with 5+ cards discard hand and draw 4 
+    {
+        //discard hand
+        while(numHandCards(state) > 0)
+        {
+            discardCard(handPos, currentPlayer, state, 0);
+        }
+
+        //draw 4
+        int i;
+        for (i = 0; i < 4; i++)
+        {
+            drawCard(currentPlayer, state);
+        }
+
+        //other players discard hand and redraw if hand size > 4
+        for (i = 0; i < state->numPlayers; i++)
+        {
+            if (i != currentPlayer)
+            {
+                if ( state->handCount[i] > 4 )
+                {
+                    //discard hand
+                    while ( state->handCount[i] > 0 )
+                    {
+                        discardCard(handPos, i, state, 0);
+                    }
+                    //draw 4
+                    for (j = 0; j < 4; j++)
+                    {
+                        drawCard(i, state);
+                    }
+                }
+            }
+        }
+
+    } 
+    return 0;
+}
+
 int updateCoins(int player, struct gameState *state, int bonus)
 {
     int i;
